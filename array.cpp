0e58ce1bea408cd8e06f05ea00672e6bf2f2690e
jcasanova1990 <jeremykcasanova@gmail.com>
2024-08-06 11:15:22 -0400

    first

---
+++ array.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int Allan = 71;
+    int Bob = 42;
+    int Steve = 36;
+    int Jesop = 23;
+    int Tomas = 3;
+
+    cout << Tomas << endl;
+    cout << Bob << endl;
+
+
+    int ages[] = { 71, 42, 36, 23, 3};
+
+    cout << ages[0] << endl;
+
+    return 0;
+}
