7e1e107da2ca4a98b1009bb0c24e3ec366de2137
jcasanova1990 <jeremykcasanova@gmail.com>
2024-08-06 11:49:32 -0400

    third

---
+++ modifyingArray.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int grades[] = {85, 95,48,100, 92};
+    cout << grades[2] << endl;
+
+    grades[2] = 88;
+    cout << grades[2] << endl;
+
+    string family[] = {"Dad", "Mom", "Brother", "Sister"};
+    int age[4];
+
+cout << family[0] << " " << age[0] << endl;
+cout << family[1] << " " << age[1] << endl;
+cout << family[2] << " " << age[2] << endl;
+cout << family[3] << " " << age[3] << endl;
+
+    return 0;
+
+}
