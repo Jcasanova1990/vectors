7211de94865b20fdb4f75cab6aa975d5e79e64f3
jcasanova1990 <jeremykcasanova@gmail.com>
2024-08-06 11:37:04 -0400

    second

---
+++ arrayAccess.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    string names[] = {"Anna", "Steve", "Dasa"};
+
+    cout << names[0] << endl;
+
+    bool bools[] = {true, false, true};
+    double decimals[] = {2.3, 4};
+    int integers[1];
+
+    cout << bools[0] << endl;
+    cout << decimals[0] << endl;
+    cout << integers[0] << endl;
+
+    return 0;
+}
